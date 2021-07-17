1864ffa3596875921913537dc7a00a77410d647d
Daniel Freitas <daniel.freitas@treevia.com.br>
2021-07-17 10:33:49 -0300

    Corrupcao no Codigo

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Daniel", "Corrêa", "123456789", "daniel@gmail.com");
+
+            student.AddSubscription(subscription);
+        }
+    }
+}
